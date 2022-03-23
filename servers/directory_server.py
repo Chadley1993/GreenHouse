c403575138df4797d69e88ed166f678189179ba6
Chadley Maggott <chadleym@bbd.co.za>
2022-03-23 15:20:40 +0200

    new directory server ready for testing

--- servers/directory_server.py
+++ servers/directory_server.py
@@ -8,6 +8,7 @@ from threading import Thread
 from flask import Flask, request
 from yaml import SafeLoader
 from requests.exceptions import InvalidSchema, ConnectionError
+from tqdm import tqdm
 
 app = Flask(__name__)
 
@@ -59,20 +60,23 @@ def get_address_book(expected_entries):
     return num_entries < expected_entries
 
 
+async def ping_server(url, i):
+    try:
+        data = requests.post(url.format(i), timeout=1)
+        reg_ip(data.json()['ip'], data.json()['user'])
+        logging.info("Found " + data.json()['user'] + " at ip address: " + data.json()['ip'])
+    except ConnectionError or InvalidSchema:
+        logging.debug("Failed to find server at: 192.168.0." + str(i))
+
+
 def find_servers(numtry=0):
     url = "http://192.168.0.{}:8202/hello"
-    addresses = [i for i in range(1, 56)]
+    addresses = [i for i in range(1, 64)]
     sub_addr = int(MYIP.split(".")[-1])
     addresses.remove(sub_addr)
 
-    for i in addresses:
-        try:
-            data = requests.post(url.format(i), timeout=1)
-            reg_ip(data.json()['ip'], data.json()['user'])
-            logging.info("Found " + data.json()['user'] + " at ip address: " + data.json()['ip'])
-        except ConnectionError or InvalidSchema:
-            logging.debug("Failed to find server at: 192.168.0." + str(i))
-            continue
+    for i in tqdm(addresses):
+        ping_server(url, i)
     
     if get_address_book(2) and numtry < 2:
         find_servers(numtry + 1)
