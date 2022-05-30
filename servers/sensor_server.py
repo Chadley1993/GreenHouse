a0a11903f4b634a0f076e048069d36659d296598
Chadley Maggott <chadleym@bbd.co.za>
2022-05-30 19:46:19 +0200

    fixed data being sent to aws

--- servers/sensor_server.py
+++ servers/sensor_server.py
@@ -158,10 +158,10 @@ def save2db(packet: SensorData, retry, try_num=0):
         return False
 
 
-def save2awsDB(data):
+def save2awsDB(sensorData: SensorData):
     dynamodb = boto3.resource('dynamodb', aws_access_key_id=os.getenv["AWS_ACCESS_KEY"], aws_secret_access_key=os.getenv["AWS_SECRET_KEY"], region_name="us-east-1")
     table = dynamodb.Table("Dev1")
-    response = table.put_item(Item=data)
+    response = table.put_item(Item={'time_stamp': sensorData.get_timestamp(), 'type': 'live-test', 'data': sensorData})
     if response["HTTPStatusCode"] != 200:
         logging.error("Save to database failed \n" + str(response))
         raise Exception("database failed")
